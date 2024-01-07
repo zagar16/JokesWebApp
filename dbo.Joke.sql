0de34e753bfebc15e159c7d5afab5ecac4f3eca0
zagar16 <zagar.domen1@gmail.com>
2024-01-07 15:31:51 +0100

    migration

---
+++ dbo.Joke.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Joke] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [JokeQuestion] NVARCHAR (MAX) NOT NULL,
+    [JokeAnswer]   NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Joke] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
